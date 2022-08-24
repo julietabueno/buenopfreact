e90c3bf3346479621a1aacb45cebe8ee41de9eb4
julietabueno <bueno.julieta.ok@gmail.com>
2022-08-24 14:29:20 -0300

    cambie el console.log que muestra cuando apretaba el boton de agregar al carrito por un sweetAlert

--- src/components/itemListContainer/ItemListContainer.jsx
+++ src/components/itemListContainer/ItemListContainer.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import { ItemCountHook } from '../ItemCount/ItemCountHook'
+import Swal from 'sweetalert2/dist/sweetalert2.js'
 import './ItemListContainer.css'
 
 
 export const ItemListContainer = ({ nickname, name, lastname, city, category = "Super Heroe" }) => {
 
     function onAdd(count) {
-        console.log(`felicitaciones agregaste ${count}`)
+        Swal.fire({
+            title: 'Listo!',
+            text: `Agregaste ${count} LEGOS al carrito.`,
+            imageUrl: 'https://w7.pngwing.com/pngs/10/228/png-transparent-yellowlego-character-lego-marvel-super-heroes-lego-minifigure-scalable-graphics-lego-s-text-smiley-emoticon.png',
+            imageWidth: 150,
+            imageHeight: 150,
+            imageAlt: 'Custom image',
+        })
     }
     return (
         <div className='classContainer'>
@@ -15,7 +23,7 @@ export const ItemListContainer = ({ nickname, name, lastname, city, category = "
             <img className='imgBatman' src="https://cdn-icons-png.flaticon.com/512/1674/1674291.png" alt="" />
             <h3>Nombre de pila: {name} {lastname}</h3>
             <h4>Profesión: {category}</h4>
-            <ItemCountHook stock={10} initial = {1} onAdd={onAdd}/>
+            <ItemCountHook stock={10} initial={1} onAdd={onAdd} />
         </div>
     )
 }
--- src/hooks/useItemCount.jsx
+++ src/hooks/useItemCount.jsx
@@ -1,20 +1,20 @@
-import {useState} from 'react'
+import { useState } from 'react'
 
-export const useItemCount = (stock,onAdd) => {
-  const [count, setCount] = useState(1);
+export const useItemCount = (stock, onAdd) => {
+    const [count, setCount] = useState(1);
 
-  function add() {
-      if (count < stock) {
-          setCount(count + 1)
-      }
-  }
+    function add() {
+        if (count < stock) {
+            setCount(count + 1)
+        }
+    }
 
-  function substract() {
-      if (count > 1) {
-          setCount(count - 1)
-      }
-  }
-  return {
-      add, substract, count
-  }
+    function substract() {
+        if (count > 1) {
+            setCount(count - 1)
+        }
+    }
+    return {
+        add, substract, count, onAdd
+    }
 }
